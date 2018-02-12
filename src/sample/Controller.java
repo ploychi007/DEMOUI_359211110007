27b786c36f7431d55d39472903cb345c9bfba593
ploychi <chawisaratpiboon@gmail.com>
2018-02-12 16:51:52 +0700

    User Login with sqlite

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,18 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+import javax.swing.*;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -11,6 +20,12 @@ public class Controller implements Initializable {
     private  loginModel loginmodel = new loginModel();
     @FXML
     private Label dbStatus;
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private Button btnLogin;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -22,4 +37,35 @@ public class Controller implements Initializable {
 
 
     }//initialize
-}
+
+
+
+
+    @FXML
+    public  void  Login(ActionEvent event){
+
+        try {
+            if (this.loginmodel.isLogin(this.username.getText(), this.password.getText())) {
+                Stage stage = (Stage) this.btnLogin.getScene().getWindow();
+                stage.close();
+
+                adminLogin();
+
+            } else {
+                JOptionPane.showMessageDialog(null,"Your username or" + "password is not corrected.");
+            }
+        }catch (Exception localException){
+            localException.printStackTrace();
+        }
+    }
+
+    private  void adminLogin(){
+        try {
+            Stage adminStage = new Stage();
+            FXMLLoader adminLoader = new FXMLLoader();
+            Pane adminRoot = adminLoader.load(getClass().getResource("/AdminDashBoerd.fxml").openStream());
+        }catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
+}//Login
